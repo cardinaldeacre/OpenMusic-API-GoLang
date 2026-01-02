2800bfe63f057c392ea4389134ab1ec5ed7289c8
cardinaldeacre <cardinaldeacre@gmail.com>
2026-01-03 06:21:13 +0700

    add jwt

---
+++ utils/jwt.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"os"
+	"time"
+)
+
+func GenerateToken(userId uint) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userId,
+		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
